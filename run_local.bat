7c33b0d497acd8f78bc6eefbd226e7f48f9f9c9c
r0ktlm <r0ktlm@users.noreply.github.com>
2026-06-12 02:46:28 +0600

    Make it pure static HTML/CSS/JS and clean up backend files

--- run_local.bat
+++ run_local.bat
@@ -1,12 +1,3 @@
 @echo off
-echo ====================================
-echo    STARTING LOCAL RETRO STREAMER    
-echo ====================================
-echo.
-echo Installing dependencies (express)...
-call npm install
-echo.
-echo Starting local server on http://localhost:3000...
-start "" "http://localhost:3000"
-node server.js
-pause
+echo Opening retro player locally...
+start index.html
